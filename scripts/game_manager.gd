f1b1151e2124ad106eb1a014b98e1ec4e12982ee
Sylvan0r <adfor409@gmail.com>
2025-12-11 18:44:10 +0000

    Añadido victoria, sonido de muerte, plataformas de hielo y mapa mejorado.

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,8 +1,15 @@
 extends Node
 
+@onready var win_label: Label = %WinLabel
 @onready var score_label: Label = %ScoreLabel
 var score = 0
 
 func add_point():
 	score += 1
 	score_label.text = "Coins: " + str(score)
+	if score == 18:
+		win_game()
+
+func win_game():
+	win_label.show()
+	win_label.text = "YOU WIN!"
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
+	audio_stream_player_2d.play()
 	timer.start()
 
 func _on_timer_timeout() -> void:
